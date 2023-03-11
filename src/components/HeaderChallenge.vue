9610b397f04dab2f95c5d88e176be8414dcd3e31
RuiCustodio96 <ruicustodio96@gmail.com>
2023-03-11 19:55:35 +0000

    User search and login API

--- src/components/HeaderChallenge.vue
+++ src/components/HeaderChallenge.vue
@@ -1,15 +1,10 @@
 <template>
-<nav>
-    <ul>
-        <li><img alt="Koerber logo" class="logo" src="@/assets/icons/korber-logo.svg" width="60" height="60" /></li>
-    </ul>
-</nav>
+    <nav>
+        <ul>
+            <li><img alt="Koerber logo" class="logo" src="@/assets/icons/korber-logo.svg" width="60" height="60" /></li>
+        </ul>
+    </nav>
 </template>
 
 <script setup lang="ts">
 </script>
-
-<style scoped>
-
-
-</style>
