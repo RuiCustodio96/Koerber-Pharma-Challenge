914fb277235273cf94c8c70b1e2270d7ea87d526
RuiCustodio96 <ruicustodio96@gmail.com>
2023-03-12 22:33:51 +0000

    Username Login instead of Email Get Posts by Email w/ Validator

--- src/components/LoginFormChallenge.vue
+++ src/components/LoginFormChallenge.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="row">
-    <div class="container">
+  <div class="container">
+    <div class="row login-form">
       <div class="card">
         <div class="card-body p-5">
 
@@ -9,8 +9,8 @@
           </div>
 
           <div class="form-outline mb-4">
-            <label class="form-label">Email</label>
-            <input type="email" class="form-input" v-model="email" />
+            <label class="form-label">Username</label>
+            <input type="text" class="form-input" v-model="username" />
             <!-- <input type="email" class="form-input" v-bind:value="email" /> -->
           </div>
 
@@ -37,11 +37,10 @@ export default {
     return {
 
       //#region Form Inputs
-      email: '',
+      username: '',
       password: '',
       //#endregion Form Inputs
 
-      username: '',
       name: '',
       userImage: ''
     };
@@ -51,41 +50,30 @@ export default {
 
     //Send user verification token to validate Login
     sendValue(data: any) {
+      if (data.message == null || data.message == undefined) {
+        this.name = data.firstName + ' ' + data.lastName;
+        this.userImage = data.image;
 
-      this.name = data.firstName + ' ' + data.lastName;
-      this.userImage = data.image;
-
-      this.$emit('log-in', this.name + ';' + this.userImage);
+        this.$emit('log-in', this.name + ';' + this.userImage);
+      } else {
+        alert("Login Failed!");
+      }
     },
 
     // Method to Login when clicked on Login Button
     async submitLogin(): Promise<void> {
 
-      //Search for the user with the given email
-      fetch('https://dummyjson.com/users/search?q=' + this.email)
+      //Login API
+      fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password,
+        })
+      })
         .then(res => res.json())
-        .then(data => {
-
-          if (data.users[0] != undefined) {
-
-            //Get information to Login
-            this.username = data.users[0].username;
-
-            //Login API
-            fetch('https://dummyjson.com/auth/login', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({
-                username: this.username,
-                password: this.password,
-              })
-            })
-              .then(res => res.json())
-              .then(data => this.sendValue(data));
-          } else {
-            alert("Login Failed!");
-          }
-        });
+        .then(data => this.sendValue(data))
     }
   }
 }
---
+++ src/components/PostsListChallenge.vue
@@ -0,0 +1,104 @@
+<template>
+    <!-- Title -->
+    <div class="container">
+        <div class="row">
+            <div class="col-7 posts-title">
+                <h3>Posts List</h3>
+            </div>
+            <div class="col-5 posts-button">
+                <button class="btn btn-primary" @click="newPost()">New Post</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- End Title -->
+
+    <!-- Form -->
+    <div class="container">
+        <div class="row">
+            <div class="col-6 email-form">
+                <label class="form-label">Insert Email</label>
+                <div class="inline">
+                    <input type="email" class="posts-email" v-on:blur="validateEmail()" v-model="email" />
+                    <button class="btn btn-primary get-posts" v-bind:disabled="invalidEmail" @click="getPosts()">Get
+                        Posts</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Form -->
+
+    <!-- Cards -->
+    <div class="container">
+        <div class="row row-cols-4">
+            <div class="col card-post" v-for="post in posts">
+                <div class="card-post-body">
+                    <div class="form-title">
+                        <h5>{{ post.title }}</h5>
+                    </div>
+
+                    <div class="form-body">
+                        <p>{{ post.body }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Cards -->
+</template>
+
+<script lang="ts">
+
+export default {
+    name: 'PostsListChallenge',
+    data() {
+        return {
+            email: '',
+            userId: '',
+            invalidEmail: true,
+            posts: Array<{ title: string, body: string }>()
+        }
+    },
+    methods: {
+
+        //Validate if valid email to enable/disable get posts button
+        validateEmail() {
+            this.invalidEmail = !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)
+        },
+
+        //Get posts for given email
+        async getPosts(): Promise<void> {
+
+            //Search for the user with the given email
+            fetch('https://dummyjson.com/users/search?q=' + this.email)
+                .then(res => res.json())
+                .then(data => {
+
+                    if (data.users[0] != undefined) {
+
+                        //Get ID to get posts
+                        this.userId = data.users[0].id;
+
+                        //Get Posts
+                        fetch('https://jsonplaceholder.typicode.com/posts?userId=' + this.userId)
+                            .then(res => res.json())
+                            .then(data => {
+                                data.forEach((element: any) => {
+                                    this.posts.push({ title: element.title, body: element.body });
+                                });
+                            });
+                    } else {
+                        //Case email doesn't exist
+                        alert("Invalid Email!");
+                        this.posts = new Array<{ title: string, body: string }>();
+                    }
+                });
+        },
+
+        newPost() {
+
+        }
+    }
+}
+
+</script>
